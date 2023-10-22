583d14e1fcef221b879f1a41b2057a8c02df45f9
Serhii-Bondariev <xx.code4u@gmail.com>
2023-10-23 01:13:14 +0300

    task-4 is completely complete..
    Time library added and configued.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import article from 'data/article.json';
 import data from 'data/data.json';
 import forbes from 'data/forbes.json';
+import transactions from 'data/transactions.json';
 
 import {
   BlogCard,
@@ -28,7 +29,7 @@ export const App = () => {
           avatar={article.avatar}
           postedAt={article.postedAt}
         />
-        
+
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
         </Heading>
@@ -40,7 +41,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,31 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+import { format } from 'date-fns';
+
+export const CryptoHistory = ({ items }) => {
+  return (
+    <>
+      <BaseTable>
+        <THead>
+          <tr>
+            <Th>№</Th>
+            <Th>PRICE</Th>
+            <Th>AMOUNT</Th>
+            <Th>DATE</Th>
+          </tr>
+        </THead>
+
+        <tbody>
+          {items.map(({ id, price, amount, date }) => (
+            <Tr key={id}>
+              <Td>{id}</Td>
+              <Td>{price}</Td>
+              <Td>{amount}</Td>
+              <Td>{format(new Date(date), 'MM/dd/yyyy, hh:mm a')}</Td>
+            </Tr>
+          ))}
+        </tbody>
+      </BaseTable>
+    </>
+  );
 };
